91fcbd02fc1be02e08d61815668c95c61d577bbe
Aurelie Miomandre <aureliemiomandre@hotmail.fr>
2019-09-25 17:57:37 +0200

    debut page selection avatar/ name

---
+++ aurelie.js
@@ -0,0 +1,14 @@
+
+const submitNameAvatar = document.getElementById("submitNameAvatar")
+const userName = document.getElementById("userName").value;
+const avatarNumber = 4
+alert(userName)
+function avatarDisplay(){
+    for (let i; i<avatarNumber, i++){
+        divAvatar = document.getElementById("avatarSelector").createElement("img");
+        img.src = "url('image/"+i+".png')"
+    }
+}
+const avatarSelector = document.getElementById("avatarSelector");
+
+submitNameAvatar.addEventListener('click',)
