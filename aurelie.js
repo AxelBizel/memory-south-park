597a093cd630f1bf41af99d5a8526a2671bed2f0
Aurelie Miomandre <aureliemiomandre@hotmail.fr>
2019-09-26 17:19:19 +0200

    username fonctionnel

--- aurelie.js
+++ aurelie.js
@@ -1,14 +1,24 @@
 
-const submitNameAvatar = document.getElementById("submitNameAvatar")
-const userName = document.getElementById("userName").value;
-const avatarNumber = 4
-alert(userName)
-function avatarDisplay(){
+// const avatarNumber = 4
+function initializeAvatar(avatarNumber){
     for (let i; i<avatarNumber, i++){
-        divAvatar = document.getElementById("avatarSelector").createElement("img");
-        img.src = "url('image/"+i+".png')"
-    }
+    divAvatar = document.getElementById("avatarSelector").createElement("img");
+    img.src = "url('image/"+i+".png')"
 }
-const avatarSelector = document.getElementById("avatarSelector");
+// function avatarDisplay(){
+//     for (let i; i<avatarNumber, i++){
+//         divAvatar = document.getElementById("avatarSelector").createElement("img");
+//         img.src = "url('image/"+i+".png')"
+//     }
+// }
+// const avatarSelector = document.getElementById("avatarSelector");
 
-submitNameAvatar.addEventListener('click',)
+let userName='toto';
+// const submitNameAvatar = document.getElementById("submitNameAvatar")
+
+
+document.getElementById("submitNameAvatar").addEventListener('click', ()=>{
+    userName = document.getElementById("userName").value
+    console.log("1",userName)
+});
+
