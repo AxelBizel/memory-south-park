5ffb8a59d9312f36b5282ca4ee32f49ea85095ff
Aurelie Miomandre <aureliemiomandre@hotmail.fr>
2019-09-27 13:29:57 +0200

    Choix avatar et pseudo ok

--- aurelie.js
+++ aurelie.js
@@ -1,29 +1,45 @@
 // Affiche les avatars (si besoin changer valeur)
-initializeAvatar(4)
+
+const avatarNumber = 4
+
+initializeAvatar(avatarNumber)
 function initializeAvatar(avatarNumber){
     for (let i=1; i<=avatarNumber; i++){
-    divAvatar = document.createElement("img");
-    divAvatar.src = "images/characters/"+i+".png"
+    divAvatar = document.createElement("li");
+    avatarUrl = "url(images/characters/"+i+".png)";
+    console.log(avatarUrl);
+    divAvatar.style.backgroundImage = avatarUrl;
+    divAvatar.style.backgroundPosition = "center center";
+    divAvatar.setAttribute("onclick", `selecter(${i})`);
+    divAvatar.setAttribute("id","avatar"+i);
+    divAvatar.style.height = "150px";
+    divAvatar.style.width = "150px";
     document.getElementById("avatarSelector").appendChild(divAvatar);
     }
 }
+// Permet de cliquer sur les avatars !
+let choosenAvatar;
+function selecter(channelNumber){
+     let listAvatar =document.getElementById("avatarSelector").getElementsByTagName("li");
+     for (let i = 0; i< avatarNumber; i++){
+         listAvatar[i].className = i+1 == channelNumber ? "selected":""; 
+     }    
+     choosenAvatar = document.getElementsByClassName("selected")[0].id
+ }
 
-
-
+ // Cree variable username et avatar en fonction de l'entrée
 let userName;
-// const submitNameAvatar = document.getElementById("submitNameAvatar")
-
-// Fonction fonctionnelle (recupere username) - besoin de rajouter des conditions (presene pseudo, choix avatar)
-// document.getElementById("submitNameAvatar").addEventListener('click', ()=>{
-//     userName = document.getElementById("userName").value
-//     console.log("1",userName)
-// });
-
 document.getElementById("submitNameAvatar").addEventListener('click', ()=>{
-    if(document.getElementById("userName").value !== ""){
-        userName = document.getElementById("userName").value
-        console.log("1",userName)
-    } else {
+    if(document.getElementById("userName").value !== "" && choosenAvatar !== undefined){
+        userName = document.getElementById("userName").value;
+        playerAvatar = choosenAvatar;
+        console.log(playerAvatar);
+        console.log("1",userName);
+    } else if (choosenAvatar !== undefined){
         alert("On a besoin de ton p'tit nom")
+    } else if (document.getElementById("userName").value !== ""){
+        alert("Choisi un avatar !")
+    } else {
+        alert("Choisi un pseudo et un avatar!")
     }
 });
