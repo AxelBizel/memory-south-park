0b7a77caf2aca3d23649a78d0ce6cc6091ba9aa2
Signupz <ludovic.mazet@gmail.com>
2019-09-27 11:18:10 +0200

    commit avant pull request Axel

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-/* DÉCOMPTE */
-
-/* DÉCOMPTE */
+let cpt = 60;
+let x;
+function decompte() {
+  if (cpt >= 0) {
+    document.getElementById("chrono").innerHTML = "La page s'affichera dans " + cpt + "sec";
+    cpt--;
+    x = setTimeout(decompte, 1000);
+  } else {
+    clearTimeout(x);
+  }
+}   
