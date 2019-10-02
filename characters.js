ec03daddd9115dd89b414d53e769342c611c2bae
Martin Rivière <martin.riviere@gmail.com>
2019-10-02 12:01:11 +0200

    renamed images and added sounds

--- characters.js
+++ characters.js
@@ -2,8 +2,8 @@ class Character {
     constructor(name, id) {
         this._name = name;
         this._id = id;
-        this._imageUrl = `./images/${name.toLowerCase()}.png`
-        this._sound = new Audio(`./sounds/${name.toLowerCase()}.wav`);
+        this._imageUrl = `./images/characters/${name.toLowerCase().replace(" ", "_").replace(".", "")}.png`
+        this._sound = new Audio(`./sounds/${name.toLowerCase().replace(" ", "_").replace(".", "")}.wav`);
     }
 
     get name() {
@@ -23,7 +23,36 @@ class Character {
     }
 }
 
+const charactersNames = [
+    'Stan',
+    'Kyle',
+    'Cartman',
+    'Kenny',
+    'Butters',
+    'Wendy',
+    'Jimmy',
+    'Token',
+    'Clyde',
+    'Mr. Mackey',
+    'Chef',
+    'Ike',
+    'Mr. Hankey',
+    'Mr. Garrison',
+    'Timmy',
+    'Servietsky',
+    'Officer Barbrady'
+];
 const characters = [];
+let index = 1;
 
-characters.push(new Character('Cartman', 1));
-characters.push(new Character('Kenny', 2));
+charactersNames.forEach(characterName => {
+    const character = new Character(characterName, index)
+    characters.push(character);
+    const imgElt = document.createElement("img");
+    imgElt.src = characters[index-1].imageUrl;
+    imgElt.addEventListener('click', () => character.playSound());
+    document.body.appendChild(imgElt);
+    index++;
+});
+
+console.log(characters[Math.floor(Math.random() * 17)]);
