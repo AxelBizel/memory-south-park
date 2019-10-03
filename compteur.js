5be08e194fd74a4b3fdf91e65c01ead059671896
Axel Bizel <axel.bizel@gmail.com>
2019-10-03 11:31:11 +0200

    compteur

--- compteur.js
+++ compteur.js
@@ -1,9 +1,6 @@
 // fonction compteur : rajoute 1 points si les cartes sont identiques. 
-
-function addPoint() {
-    const counter = document.getElementById('counter');
-    nbPoints++
-    counter.innerText = nbPoints;    
+function addPoint () {
+    const counter = document.getElementById('counter'); 
+    nbPoints++;
+    counter.innerText= nbPoints;    
 }
-
-addPoint(true);
