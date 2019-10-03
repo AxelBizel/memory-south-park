b2f87b81a34473298c707dd05c8164039da1445f
Signupz <50277760+Signupz@users.noreply.github.com>
2019-10-03 16:42:44 +0200

    Update compteur.js

--- compteur.js
+++ compteur.js
@@ -1,11 +1,4 @@
 // fonction compteur : rajoute 1 points si les cartes sont identiques. 
-<<<<<<< HEAD
-function addPoint () {
-    const counter = document.getElementById('counter'); 
-    nbPoints++;
-    counter.innerText= nbPoints;    
-}
-=======
 
 function addPoint() {
     const counter = document.getElementById('counter');
@@ -13,4 +6,3 @@ function addPoint() {
     counter.innerText = nbPoints;
     return nbPoints; 
 }
->>>>>>> 642aaaff01de83070adb096b2ab4c948b826b58b
