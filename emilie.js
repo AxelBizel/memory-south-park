3a6a2f9f6bdb8734be132171c80dde00de4d7b15
Emilie Pujol <emilie.pujol@protonmail.com>
2019-10-04 10:24:27 +0200

    fenêtre ouverture + option 2 joueurs

--- emilie.js
+++ emilie.js
@@ -4,25 +4,18 @@ const submitNameAvatar = document.getElementById("submitNameAvatar")
 const playerSelector = document.getElementById("playerSelector")
 const diffSelector = document.getElementById("diffSelector")
 const playerTwo = document.getElementById("playerTwo")
-const avatarSelectorWrap = document.getElementById("avatarSelectorWrap")
-const avatarSelectorWrap2 = document.getElementById("avatarSelectorWrap2")
+const pseudoAvatarSelector1Player = document.getElementById("divPseudoAvatarSelector1Player")
+const pseudoAvatarSelector2Players = document.getElementById("divPseudoAvatarSelector2Players")
 
 firstBtn.addEventListener('click', (event) => {
-    openingForm.style.display = 'none';
-    avatarSelectorWrap.style.display = 'block'; 
-})
+    openingForm.style.display = 'none';})
 
 playerSelector.addEventListener('change', (event) => {
     if (playerSelector.value === "twoPlayers") {
-        avatarSelectorWrap2.style.display = 'block'
+        pseudoAvatarSelector2Players.style.display = 'block'
  }
-})
-
-diffSelector.addEventListener('change', () => {
-    if (playerSelector.value === "easy") {
-    }
-    if (playerSelector.value === "difficult") {
-
+    else {
+        pseudoAvatarSelector1Player = 'block'
     }
 })
 
